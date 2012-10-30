f8649c3b37096e2b6924619e289516c7cc47ed85
builtbyme <support@macroiq.com>
2012-10-30 16:04:54 +1030

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '775cd64ddc1a9916f6ae50687024647e59159ebcdb65d4ab188779eab9754ac91fd99ca5e04600911f61b0fa748dda5fe44009513abc0c770c9748a87f9047a6'
